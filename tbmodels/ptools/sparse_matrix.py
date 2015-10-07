a132ff1656873a6343c1c7c52fbc295097eeb4f6
Dominik Gresch <greschd@ethz.ch>
2015-10-07 13:20:52 +0200

    moved sparse_matrix into the repo

---
+++ tbmodels/ptools/sparse_matrix.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@gmx.ch>
+# Date:    06.10.2015 17:35:33 CEST
+# File:    sparse_matrix.py
+
+import numpy as np
+import scipy.sparse as sp
+
+class ArrayConvertible(object):
+    def __array__(self):
+        return self.toarray()
+
+    def transpose(self):
+        return self.__class__(super(ArrayConvertible, self).transpose())
+
+    def conjugate(self):
+        return self.__class__(super(ArrayConvertible, self).conjugate())
+    
+
+class csr(ArrayConvertible, sp.csr_matrix):
+    pass
+    
+class coo(ArrayConvertible, sp.coo_matrix):
+    pass
+
+class lil(ArrayConvertible, sp.lil_matrix):
+    pass
+
+
+if __name__ == "__main__":
+    a = csr([[0, 1j], [0, 0]])
+    #~ print(a.conjugate())
+    print(np.array(a.conjugate()))
+    print(np.array(a.transpose()))
+    print("sparse_matrix.py")
