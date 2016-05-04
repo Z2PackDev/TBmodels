45be3a1aad0b39b9895199036ed65a1fb45cfc3c
Dominik Gresch <greschd@gmx.ch>
2016-05-04 14:48:41 +0200

    translated uc test into pytest form

--- tests_new/test_uc_change.py
+++ tests_new/test_uc_change.py
@@ -10,6 +10,7 @@ import numpy as np
 
 import tbmodels
 
+
 @pytest.fixture
 def get_model():
     def inner(t1, t2, dim=3, uc=None, pos=None):
@@ -24,7 +25,7 @@ def get_model():
             uc=uc,
             dim=3
         )
-        
+
         for phase, G in zip([1, -1j, 1j, -1], tbmodels.helpers.combine([0, -1], [0, -1], 0)):
             model.add_hop(t1 * phase, 0, 1, G)
 
@@ -35,18 +36,21 @@ def get_model():
         return model
     return inner
 
+
 def test_no_uc_unity(get_model):
     model = get_model(0.1, 0.2)
     new_model = model.change_uc(uc=np.identity(3))
     assert new_model.uc == model.uc
     assert np.isclose(model.pos, new_model.pos).all()
 
+
 def test_uc_unity(get_model):
     model = get_model(0.1, 0.2, uc=np.diag([1, 2, 3]))
     new_model = model.change_uc(uc=np.identity(3))
     assert np.isclose(new_model.uc, model.uc).all()
     assert np.isclose(model.pos, new_model.pos).all()
-            
+
+
 def test_uc_1(get_model):
     model = get_model(0.1, 0.2, uc=np.diag([1, 2, 3]))
     new_model = model.change_uc(uc=np.array([[1, 2, 0], [0, 1, 3], [0, 0, 1]]))
@@ -63,7 +67,8 @@ def test_uc_1(get_model):
     ])
     assert np.isclose(new_model.uc, res_uc).all()
     assert np.isclose(new_model.pos, res_pos).all()
-            
+
+
 def test_uc_2(get_model):
     model = get_model(0.1, 0.2, uc=np.array([[0, 1, 0], [2, 0, 1], [1, 0, 1]]))
     new_model = model.change_uc(uc=np.array([[1, 2, 0], [0, 1, 3], [0, 0, 1]]))
@@ -80,38 +85,45 @@ def test_uc_2(get_model):
     ])
     assert np.isclose(new_model.uc, res_uc).all()
     assert np.isclose(new_model.pos, res_pos).all()
-        
-    #~ def test_uc_hamilton_unity(get_model):
-        #~ model = get_model(0.1, 0.2, uc=np.diag([1, 2, 3]))
-        #~ new_model = model.change_uc(uc=np.identity(3))
 
-        #~ res = array([[ 1.0+0.j       ,  0.2+0.1618034j,  0.0+0.j       ],
-       #~ [ 0.2-0.1618034j, -1.0+0.j       ,  0.0+0.j       ],
-       #~ [ 0.0+0.j       ,  0.0+0.j       ,  0.0+0.j       ]])
-        #~ assert np.isclose(res, new_model.hamilton([0.1, 0.4, 0.7]))
-        
-    #~ def test_uc_hamilton_1(get_model):
-        #~ model = get_model(0.1, 0.2, uc=np.diag([1, 2, 3]))
-        #~ new_model = model.change_uc(uc=np.array([[1, 2, 0], [0, 1, 3], [0, 0, 1]]))
 
-        #~ res = array([[ 1.44721360+0.j        ,  0.00877853-0.17298248j,  0.00000000+0.j        ],
-       #~ [ 0.00877853+0.17298248j, -1.44721360+0.j        ,  0.00000000+0.j        ],
-       #~ [ 0.00000000+0.j        ,  0.00000000+0.j        ,  0.00000000+0.j        ]])
-        #~ assert np.isclose(res, new_model.hamilton([0.1, 0.4, 0.7]))
-        
-    #~ def test_uc_hamilton_2(get_model):
-        #~ model = get_model(0.1, 0.2, uc=np.array([[0, 1, 0], [2, 0, 1], [1, 0, 1]]))
-        #~ new_model = model.change_uc(uc=np.array([[1, 2, 0], [0, 1, 3], [0, 0, 1]]))
+def test_uc_hamilton_unity(get_model):
+    model = get_model(0.1, 0.2, uc=np.diag([1, 2, 3]))
+    new_model = model.change_uc(uc=np.identity(3))
 
-        #~ res = array([[ 1.44721360+0.j        ,  0.00877853-0.17298248j,  0.00000000+0.j        ],
-       #~ [ 0.00877853+0.17298248j, -1.44721360+0.j        ,  0.00000000+0.j        ],
-       #~ [ 0.00000000+0.j        ,  0.00000000+0.j        ,  0.00000000+0.j        ]])
-        #~ assert np.isclose(res, new_model.hamilton([0.1, 0.4, 0.7]))
-            
-    #~ def test_error(get_model):
-        #~ model = get_model(0.1, 0.2, uc=np.identity(3))
-        #~ self.assertRaises(ValueError, model.change_uc, uc=np.diag([2, 1, 1]))
-    
+    res = np.array([
+        [1.0 + 0.j,  0.2 + 0.1618034j,  0.0 + 0.j],
+        [0.2 - 0.1618034j, -1.0 + 0.j,  0.0 + 0.j],
+        [0.0 + 0.j,  0.0 + 0.j,  0.0 + 0.j]
+    ])
+    assert np.isclose(res, new_model.hamilton([0.1, 0.4, 0.7])).all()
 
-#~ if __name__ == "__main__":
-    #~ unittest.main()
+
+def test_uc_hamilton_1(get_model):
+    model = get_model(0.1, 0.2, uc=np.diag([1, 2, 3]))
+    new_model = model.change_uc(uc=np.array([[1, 2, 0], [0, 1, 3], [0, 0, 1]]))
+
+    res = np.array([
+        [1.44721360 + 0.j,  0.00877853 - 0.17298248j,  0.00000000 + 0.j],
+        [0.00877853 + 0.17298248j, -1.44721360 + 0.j,  0.00000000 + 0.j],
+        [0.00000000 + 0.j,  0.00000000 + 0.j,  0.00000000 + 0.j]
+    ])
+    assert np.isclose(res, new_model.hamilton([0.1, 0.4, 0.7])).all()
+
+
+def test_uc_hamilton_2(get_model):
+    model = get_model(0.1, 0.2, uc=np.array([[0, 1, 0], [2, 0, 1], [1, 0, 1]]))
+    new_model = model.change_uc(uc=np.array([[1, 2, 0], [0, 1, 3], [0, 0, 1]]))
+
+    res = np.array([
+        [1.44721360 + 0.j,  0.00877853 - 0.17298248j,  0.00000000 + 0.j],
+        [0.00877853 + 0.17298248j, -1.44721360 + 0.j,  0.00000000 + 0.j],
+        [0.00000000 + 0.j,  0.00000000 + 0.j,  0.00000000 + 0.j]
+    ])
+    assert np.isclose(res, new_model.hamilton([0.1, 0.4, 0.7])).all()
+
+
+def test_error(get_model):
+    model = get_model(0.1, 0.2, uc=np.identity(3))
+    with pytest.raises(ValueError):
+        model.change_uc(uc=np.diag([2, 1, 1]))
