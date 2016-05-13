005b9792844793858379014d611f5a6403b969dc
Dominik Gresch <greschd@gmx.ch>
2016-05-13 15:42:56 +0200

    continued work on new tests

--- tests_new/conftest.py
+++ tests_new/conftest.py
@@ -8,6 +8,7 @@
 import json
 import pytest
 import numbers
+import contextlib
 from collections.abc import Iterable
 try:
     from functools import singledispatch
@@ -18,10 +19,7 @@ import numpy as np
 
 import tbmodels
 
-@pytest.fixture
-def test_name(request):
-    """Returns module_name.function_name for a given test"""
-    return request.module.__name__ + '/' + request._parent_request._pyfuncitem.name
+#--------------------------ENCODING-------------------------------------#
 
 @singledispatch
 def encode(obj):
@@ -52,17 +50,59 @@ def _(obj):
 def _(obj):
     return list(obj)
         
+#--------------------------DECODING-------------------------------------#
+
+@singledispatch
+def decode(obj):
+    return obj
+
+@decode.register(str)
+def _(obj):
+    return obj
+
+@decode.register(Iterable)
+def _(obj):
+    return [decode(x) for x in obj]
+
+def decode_complex(obj):
+    return complex(obj['real'], obj['imag'])
+
+
+@decode.register(dict)
+def _(obj):
+    with contextlib.suppress(AttributeError):
+        obj = {k.decode('utf-8'): v for k, v in obj.items()}
+    special_markers = [key for key in obj.keys() if key.startswith('__')]
+    if len(special_markers) == 1:
+        name = special_markers[0].strip('__')
+        return globals()['decode_' + name](obj)
+    else:
+        return obj
+
+#--------------------------FIXTURES-------------------------------------#
+
+@pytest.fixture
+def test_name(request):
+    """Returns module_name.function_name for a given test"""
+    return request.module.__name__ + '/' + request._parent_request._pyfuncitem.name
+
 @pytest.fixture
 def compare_data(request, test_name, scope="session"):
     """Returns a function which either saves some data to a file or (if that file exists already) compares it to pre-existing data using a given comparison function."""
     def inner(compare_fct, data, tag=None):
         full_name = test_name + (tag or '')
-        val = request.config.cache.get(full_name, None)
+        
+        # get rid of json-specific quirks
+        # store as string because I cannot add the decoder to the pytest cache
+        data_str = json.dumps(data, default=encode)
+        data = json.loads(data_str, object_hook=decode)
+        val = json.loads(request.config.cache.get(full_name, 'null'), object_hook=decode)
+
         if val is None:
-            request.config.cache.set(full_name, json.loads(json.dumps(data, default=encode)))
+            request.config.cache.set(full_name, data_str)
             raise ValueError('Reference data does not exist.')
         else:
-            assert compare_fct(val, json.loads(json.dumps(data, default=encode))) # get rid of json-specific quirks
+            assert compare_fct(val, data) 
     return inner
 
 @pytest.fixture
---
+++ tests_new/test_hr.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@gmx.ch>
+# Date:    05.05.2015 13:59:00 CEST
+# File:    hr_hamilton.py
+
+import pytest
+
+import tbmodels
+import numpy as np
+
+kpt = [(0.1, 0.2, 0.7), (-0.3, 0.5, 0.2), (0., 0., 0.), (0.1, -0.9, -0.7)]
+
+def test_hr(compare_data):
+    model = tbmodels.HrModel('./samples/hr_hamilton.dat', occ=28)
+    H_list = np.array([model.hamilton(k) for k in kpt])
+
+    compare_data(lambda x, y: np.isclose(x, y).all(), H_list)
+        
+def test_error():
+    with pytest.raises(ValueError):
+        tbmodels.HrModel('./samples/hr_hamilton.dat', occ=28, pos=[[1., 1., 1.]])
