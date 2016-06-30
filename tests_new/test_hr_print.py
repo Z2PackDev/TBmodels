bab7110381d8402a8bb39de0db48ef844ebcbd45
Dominik Gresch <greschd@gmx.ch>
2016-06-30 15:28:15 +0200

    moved hr consistency test to new test suite

--- tests_new/test_hr_print.py
+++ tests_new/test_hr_print.py
@@ -18,4 +18,9 @@ def test_hr_print(t, get_model, compare_equal):
     model = get_model(*t)
     compare_equal(model.to_hr().splitlines()[1:]) # timestamp in first line isn't equal
     
-
+def test_consistency():
+    model = tbmodels.HrModel('./samples/hr_hamilton.dat', occ=28)
+    lines_new = model.to_hr().split('\n')
+    with open('./samples/hr_hamilton.dat', 'r') as f:
+        lines_old = [line.rstrip(' \r\n') for line in f.readlines()]
+    assert lines_new[1:] == lines_old[1:]
