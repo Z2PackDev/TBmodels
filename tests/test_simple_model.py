473f0e765bdc209c44060f0a80ca6c8a0dd67895
Dominik Gresch <greschd@gmx.ch>
2016-07-06 03:01:45 +0200

    added hamilton and eigenval test to simple model test

--- tests/test_simple_model.py
+++ tests/test_simple_model.py
@@ -12,6 +12,10 @@ from parameters import T_VALUES, KPT
 
 @pytest.mark.parametrize('t1', T_VALUES)
 @pytest.mark.parametrize('t2', T_VALUES)
-def test_add(t1, t2, get_model, compare_data, models_equal):
+@pytest.mark.parametrize('k', KPT)
+def test_simple(t1, t2, get_model, k, compare_data, models_equal, compare_equal):
     m = get_model(*t1)
+    
+    compare_equal(m.hamilton(k), tag='hamilton')
+    compare_equal(m.eigenval(k), tag='eigenval')
     compare_data(models_equal, m)
