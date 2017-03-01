22565afe0c37a707dbce662230d2b9621c82fb6b
Dominik Gresch <greschd@gmx.ch>
2017-03-01 14:16:06 +0100

    got a first working version of the symmetrization example

--- examples/symmetrization/symmetrize.py
+++ examples/symmetrization/symmetrize.py
@@ -3,6 +3,7 @@
 #
 # Author:  Georg Winkler, Dominik Gresch <greschd@gmx.ch>
 
+import sys
 import random
 
 import numpy as np
@@ -13,6 +14,124 @@ import tbmodels as tb
 from tbmodels.helpers import SymmetryOperation, Representation
 
 import pymatgen as mg
+
+def getpath(P, n, b=None):
+    """ Calculates path in k-space given symmetry points P, length of
+    a segment n and reciprocal lattice vectors b """
+    P = np.array(P)
+    if b is None:
+        b = np.eye(P.shape[1])
+    l = len(P)
+    path = np.zeros(((l - 1) * n + 1, P.shape[1]))
+    for i in range(0, l - 1):
+        for j in range(0, n):
+            p = P[i, :] + (P[i + 1, :] - P[i, :]) * float(j) / (n)
+            path[i * n + j, :] = np.dot(p, b)
+    path[-1, :] = np.dot(P[-1], b)
+    return path
+
+
+def getdist(path, b=None):
+    """ Calculates distance from a k-path """
+    dist = np.zeros(len(path))
+    if b is None:
+        for i in range(1, len(path)):
+            dist[i] = dist[i - 1] + la.norm(path[i] - path[i - 1])
+    else:
+        for i in range(1, len(path)):
+            dist[i] = dist[i - 1] + la.norm(dot(path[i] - path[i - 1], b))
+    return dist
+
+
+def calcEpath(path, model, verbose=False, give_states=False, nsparse=None, modelargs={}, solverargs={}):
+    """ Calculates energy eigenvalues along path """
+    E = []
+    V = []
+    # if hasattr(model, 'hamilton'):
+    H = model.hamilton
+    # elif hasattr(model, '__call__'):
+    #     H = model
+    # if nsparse is None:
+    #     solver = lambda k: la.eigh(H(k, **modelargs), **solverargs)
+    # else:
+    #     solver = lambda k: sp.linalg.eigsh(
+    #         H(k, **modelargs), k=nsparse, **solverargs)
+    solver = lambda k: la.eigh(model.hamilton(k))
+    if not verbose:
+        for k in path:
+            Ei, Vi = solver(k)
+            E.append(Ei)
+            if give_states:
+                V.append(Vi)
+    else:
+        l = len(path)
+        print('')
+        for i, k in enumerate(path):
+            sys.stdout.write('\r')
+            sys.stdout.write(str(i) + '/' + str(l))
+            sys.stdout.flush()
+            Ei, Vi = solver(k)
+            E.append(Ei)
+            if give_states:
+                V.append(Vi)
+        print('')
+    if give_states:
+        return np.array(E), np.array(V)
+    else:
+        return np.array(E)
+
+
+def spin_reps(prep):
+    """
+    Calculates the spin rotation matrices. The formulas to determine the rotation axes and angles
+    are taken from `here <http://scipp.ucsc.edu/~haber/ph116A/rotation_11.pdf>`_.
+
+    :param prep:   List that contains 3d rotation matrices.
+    :type prep:    list(array)
+    """
+    # general representation of the D1/2 rotation about the axis (l,m,n) around the
+    # angle phi
+    D12 = lambda l, m, n, phi: np.array([[np.cos(phi / 2.) - 1j * n * np.sin(phi / 2.), (-1j * l - m) * np.sin(phi / 2.)],
+                                         [(-1j * l + m) * np.sin(phi / 2.), np.cos(phi / 2.) + 1j * n * np.sin(phi / 2.)]])
+
+    n = np.zeros(3)
+    tr = np.trace(prep)
+    det = np.round(np.linalg.det(prep), 5)
+    if det == 1.:  # rotations
+        theta = np.arccos(0.5 * (tr - 1.))
+        if theta != 0:
+            n[0] = prep[2, 1] - prep[1, 2]
+            n[1] = prep[0, 2] - prep[2, 0]
+            n[2] = prep[1, 0] - prep[0, 1]
+            if np.round(np.linalg.norm(n), 5) == 0.:  # theta = pi, that is C2 rotations
+                e, v = la.eig(prep)
+                n = v[:, list(np.round(e, 10)).index(1.)]
+                spin = np.round(D12(n[0], n[1], n[2], np.pi), 15)
+            else:
+                n /= np.linalg.norm(n)
+                spin = np.round(D12(n[0], n[1], n[2], theta), 15)
+        else:  # case of unitiy
+            spin = D12(0, 0, 0, 0)
+    elif det == -1.:  # improper rotations and reflections
+        theta = np.arccos(0.5 * (tr + 1.))
+        if np.round(theta, 5) != np.round(np.pi, 5):
+            n[0] = prep[2, 1] - prep[1, 2]
+            n[1] = prep[0, 2] - prep[2, 0]
+            n[2] = prep[1, 0] - prep[0, 1]
+            if np.round(np.linalg.norm(n), 5) == 0.:  # theta = 0 (reflection)
+                e, v = la.eig(prep)
+                # normal vector is eigenvector to eigenvalue -1
+                n = v[:, list(np.round(e, 10)).index(-1.)]
+                # spin is a pseudovector!
+                spin = np.round(D12(n[0], n[1], n[2], np.pi), 15)
+            else:
+                n /= np.linalg.norm(n)
+                # rotation followed by reflection:
+                spin = np.round(
+                    np.dot(D12(n[0], n[1], n[2], np.pi), D12(n[0], n[1], n[2], theta)), 15)
+        else:  # case of inversion (does not do anything to spin)
+            spin = D12(0, 0, 0, 0)
+    return np.array(spin)
 
 if __name__ == '__main__':
     HDF5_FILE = 'data/model_nosym.hdf5'
@@ -37,57 +156,7 @@ if __name__ == '__main__':
         )
     )
 
-    def spin_reps(prep):
-        """
-        Calculates the spin rotation matrices. The formulas to determine the rotation axes and angles
-        are taken from `here <http://scipp.ucsc.edu/~haber/ph116A/rotation_11.pdf>`_.
 
-        :param prep:   List that contains 3d rotation matrices.
-        :type prep:    list(array)
-        """
-        # general representation of the D1/2 rotation about the axis (l,m,n) around the
-        # angle phi
-        D12 = lambda l, m, n, phi: np.array([[np.cos(phi / 2.) - 1j * n * np.sin(phi / 2.), (-1j * l - m) * np.sin(phi / 2.)],
-                                             [(-1j * l + m) * np.sin(phi / 2.), np.cos(phi / 2.) + 1j * n * np.sin(phi / 2.)]])
-
-        n = np.zeros(3)
-        tr = np.trace(prep)
-        det = np.round(np.linalg.det(prep), 5)
-        if det == 1.:  # rotations
-            theta = np.arccos(0.5 * (tr - 1.))
-            if theta != 0:
-                n[0] = prep[2, 1] - prep[1, 2]
-                n[1] = prep[0, 2] - prep[2, 0]
-                n[2] = prep[1, 0] - prep[0, 1]
-                if np.round(np.linalg.norm(n), 5) == 0.:  # theta = pi, that is C2 rotations
-                    e, v = la.eig(prep)
-                    n = v[:, list(np.round(e, 10)).index(1.)]
-                    spin = np.round(D12(n[0], n[1], n[2], np.pi), 15)
-                else:
-                    n /= np.linalg.norm(n)
-                    spin = np.round(D12(n[0], n[1], n[2], theta), 15)
-            else:  # case of unitiy
-                spin = D12(0, 0, 0, 0)
-        elif det == -1.:  # improper rotations and reflections
-            theta = np.arccos(0.5 * (tr + 1.))
-            if np.round(theta, 5) != np.round(np.pi, 5):
-                n[0] = prep[2, 1] - prep[1, 2]
-                n[1] = prep[0, 2] - prep[2, 0]
-                n[2] = prep[1, 0] - prep[0, 1]
-                if np.round(np.linalg.norm(n), 5) == 0.:  # theta = 0 (reflection)
-                    e, v = la.eig(prep)
-                    # normal vector is eigenvector to eigenvalue -1
-                    n = v[:, list(np.round(e, 10)).index(-1.)]
-                    # spin is a pseudovector!
-                    spin = np.round(D12(n[0], n[1], n[2], np.pi), 15)
-                else:
-                    n /= np.linalg.norm(n)
-                    # rotation followed by reflection:
-                    spin = np.round(
-                        np.dot(D12(n[0], n[1], n[2], np.pi), D12(n[0], n[1], n[2], theta)), 15)
-            else:  # case of inversion (does not do anything to spin)
-                spin = D12(0, 0, 0, 0)
-        return np.array(spin)
 
     structure = mg.Structure.from_file('data/POSCAR')
 
@@ -98,16 +167,13 @@ if __name__ == '__main__':
     rots = [x.rotation_matrix for x in symops]
     taus = [x.translation_vector for x in symops]
 
-    # print(symops)
-    # sys.exit()
-
     # get corresponding represesentations in the Hamiltonian basis
     reps = []
     for n, (rot, tau) in enumerate(zip(rots, taus)):
         C = symops_cart[n].rotation_matrix
         tauc = symops_cart[n].translation_vector
         prep = C
-        spinrep = spin_reps(C).conj()
+        spinrep = spin_reps(C)
         R = np.kron(spinrep, la.block_diag(1., prep, prep))
         reps.append(R)
 
@@ -116,7 +182,7 @@ if __name__ == '__main__':
             kmatrix=rot.transpose(),
             repr=Representation(
                 complex_conjugate=False,
-                matrix=repr_mat.conj()
+                matrix=repr_mat
             )
         )
         for rot, repr_mat in zip(rots, reps)
@@ -124,27 +190,31 @@ if __name__ == '__main__':
 
     model = model_nosym.symmetrize([time_reversal] + symmetries)
 
-    # for R in reps:
-    #     assert np.isclose(R.conjugate().transpose(), la.inv(R)).all()
-
     # print(model.eigenval(k))
     # print(model_nosym.eigenval(k))
-    # assert np.isclose(model_nosym.eigenval(k), model.eigenval(k), atol=1e-3).all()
+    k = (0.12351251235, 0.63246213, 0.125165)
+    assert np.isclose(model_nosym.eigenval(k), model.eigenval(k), atol=3e-2).all()
 
-    # efermi = la.eigh(tbm.H([0, 0, 0]))[0][tbm.nocc]
-    # nsteps = 50
-    # path = getpath(np.array([L, G, X, L, W, G, U]), nsteps, b)
-    # dist = getdist(path)
-    # E = calcEpath(path, tbm_SO, verbose=True)
-    # # E1 = calcEpath(path,Hsym,verbose=True)
-    # E1 = calcEpath(path, tbm_sym_SO, verbose=True)
-    #
-    # plt.figure()
-    # plt.plot(dist, E - efermi, 'k')
-    # plt.plot(dist, E1 - efermi, 'r--')
-    # for x in dist[::nsteps]:
-    #     plt.axvline(x, color='b')
-    # plt.xticks(dist[::nsteps], ['L', 'G', 'X', 'L', 'W', 'G', 'U'])
-    # plt.xlim([dist[0], dist[-1]])
-    # plt.ylim([-6, 6])
-    # plt.savefig(folder + 'compare_bands.pdf')
+    # band structure
+    G = np.array([0., 0., 0.])
+    L = np.array([0.5, 0.5, 0.5])
+    X = np.array([.5, 0., 0.5])
+    W = np.array([0.5, 0.25, 0.75])
+    U = np.array([0.25, 0.625, 0.625])
+
+    efermi = model.eigenval([0, 0, 0])[model.occ]
+    nsteps = 50
+    path = getpath(np.array([L, G, X, L, W, G, U]), nsteps, model.reciprocal_lattice)
+    dist = getdist(path)
+    E = calcEpath(path, model_nosym, verbose=True)
+    E1 = calcEpath(path, model, verbose=True)
+
+    plt.figure()
+    plt.plot(dist, E - efermi, 'k')
+    plt.plot(dist, E1 - efermi, 'r--')
+    for x in dist[::nsteps]:
+        plt.axvline(x, color='b')
+    plt.xticks(dist[::nsteps], ['L', 'G', 'X', 'L', 'W', 'G', 'U'])
+    plt.xlim([dist[0], dist[-1]])
+    plt.ylim([-6, 6])
+    plt.savefig('plots/compare_bands.pdf')
