93926f75715ad989db3ec110b0f44c1fdd0e2448
Dominik Gresch <greschd@gmx.ch>
2017-03-16 10:10:14 +0100

    added example for command-line parsing of tight-binding models

---
+++ examples/cmdline_parser/parse_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@gmx.ch>
+
+"""
+This script parses a tight-binding model from Wannier90 output and creates the corresponding HDF5 file.
+"""
+
+import os
+import sys
+import tbmodels
+
+usage = "./parse_model.py <input_folder/prefix> <output_name>"
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(usage)
+        sys.exit()
+
+    common_path = sys.argv[1]
+    input_files = dict()
+    input_files['hr_file'] = os.path.join(common_path, '_hr.dat')
+
+    for key, suffix in [
+            ('win_file', '.win'),
+            ('wsvec_file', '_wsvec.dat'),
+            ('xyz_file', '_centres.xyz'),
+    ]:
+        filename = os.path.join(common_path, suffix)
+        if os.path.isfile(filename):
+            input_files[key] = filename
+
+    model = tbmodels.Model.from_wannier_files(ignore_orbital_order=True, **input_files)
+    model.to_hdf5_file(sys.argv[2] + '.hdf5')
