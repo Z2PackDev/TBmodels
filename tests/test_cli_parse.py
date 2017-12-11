0168836c6bb2c04ac7a9d4ac6682fca47512ea4c
Dominik Gresch <greschd@gmx.ch>
2017-12-11 18:29:22 +0100

    Add tests for parse CLI with pos_kind

--- tests/test_cli_parse.py
+++ tests/test_cli_parse.py
@@ -9,19 +9,21 @@ import tbmodels
 from tbmodels._cli import cli
 
 
+@pytest.mark.parametrize('pos_kind', ['wannier', 'nearest_atom'])
 @pytest.mark.parametrize('prefix', ['silicon', 'bi'])
-def test_cli_parse(models_equal, prefix, sample):
+def test_cli_parse(models_equal, prefix, sample, pos_kind):
     runner = CliRunner()
     with tempfile.NamedTemporaryFile() as out_file:
         run = runner.invoke(
-            cli,
-            ['parse', '-o', out_file.name, '-f',
-             sample(''), '-p', prefix],
+            cli, [
+                'parse', '-o', out_file.name, '-f',
+                sample(''), '-p', prefix, '--pos-kind', pos_kind
+            ],
             catch_exceptions=False
         )
         print(run.output)
         model_res = tbmodels.Model.from_hdf5_file(out_file.name)
     model_reference = tbmodels.Model.from_wannier_folder(
-        folder=sample(''), prefix=prefix
+        folder=sample(''), prefix=prefix, pos_kind=pos_kind
     )
     models_equal(model_res, model_reference)
