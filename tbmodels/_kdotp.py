Fix computation of Hamiltonian for k.p models.

tbmodels/_kdotp.py
@@ -6,7 +6,7 @@ from fsc.hdf5_io import subscribe_hdf5, SimpleHDF5Mapping
 
 
 @export
-@subscribe_hdf5('tbmodels.model', check_on_load=False)
+@subscribe_hdf5('tbmodels.kdotp_model', check_on_load=False)
 class KdotpModel(SimpleHDF5Mapping):
     HDF5_ATTRIBUTES = ['taylor_coefficients']
 
@@ -17,7 +17,7 @@ class KdotpModel(SimpleHDF5Mapping):
         }
 
     def hamilton(self, k):
-        return sum(sum(kval**p for kval, p in zip(k, pow)) * mat for pow, mat in self.taylor_coefficients.items())
+        return sum(np.prod(np.array(k)**np.array(pow)) * mat for pow, mat in self.taylor_coefficients.items())
 
     def eigenval(self, k):
         return la.eigvalsh(self.hamilton(k))