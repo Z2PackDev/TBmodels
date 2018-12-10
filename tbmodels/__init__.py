024a1d90ae890450167f697d97aa166dca3c0c5f
Dominik Gresch <greschd@gmx.ch>
2018-12-10 11:14:36 +0100

    Remove beta tag from version.

--- tbmodels/__init__.py
+++ tbmodels/__init__.py
@@ -2,7 +2,7 @@ r"""
 TBmodels is a tool for creating / loading and manipulating tight-binding models.
 """
 
-__version__ = '1.3.0b1'
+__version__ = '1.3.0'
 
 # import order is important due to circular imports
 from . import helpers
