e9a6c67ac16894412c60f16e2c7f7c935e026ef3
Dominik Gresch <greschd@gmx.ch>
2019-01-21 21:00:48 +0100

    Bump bugfix number.

--- tbmodels/__init__.py
+++ tbmodels/__init__.py
@@ -4,7 +4,7 @@ r"""
 TBmodels is a tool for creating / loading and manipulating tight-binding models.
 """
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 # import order is important due to circular imports
 from . import helpers
