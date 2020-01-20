3453d62aa58de073bd1999f85fd39441971cd015
Dominik Gresch <greschd@gmx.ch>
2020-01-20 13:39:39 +0100

    Fix pre-commit issues in kdotp test.

--- tests/test_kdotp.py
+++ tests/test_kdotp.py
@@ -3,6 +3,9 @@
 
 # (c) 2015-2018, ETH Zurich, Institut fuer Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
+"""
+Test the construction of k.p models around a given k-point.
+"""
 
 import pytest
 import numpy as np
@@ -13,6 +16,7 @@ from parameters import KPT
 
 
 def test_kdotp_model():
+    """Check that a k.p model can be explicitly created."""
     model = KdotpModel({(0, 0): np.eye(2), (1, 0): [[0, 1j], [-1j, 0]], (0, 2): [[0, 1], [1, 0]]})
 
     assert np.allclose(model.hamilton((0, 0)), np.eye(2))
@@ -23,6 +27,10 @@ def test_kdotp_model():
 
 
 def test_raises_not_hermitian():
+    """
+    Test that the k.p model constructor raises when trying to construct
+    model with a non-hermitian Hamiltonian.
+    """
     with pytest.raises(ValueError):
         KdotpModel({(0, 0): [[0, 1], [2, 0]]})
 
@@ -30,13 +38,21 @@ def test_raises_not_hermitian():
 @pytest.mark.parametrize('kpt', KPT)
 @pytest.mark.parametrize('order', [0, 1, 2, 3])
 def test_construct_kdotp(sample, kpt, order):
+    """
+    Test constructing a k.p model around a given k-point of an InAs
+    tight-binding model.
+    """
     model_tb = tbmodels.io.load(sample('InAs_nosym.hdf5'))
     model_kp = model_tb.construct_kdotp(kpt, order=order)
 
     assert np.allclose(model_kp.eigenval((0, 0, 0)), model_tb.eigenval(kpt))
 
 
-def test_construct_kdotp_negative_order(sample):
+def test_construct_kdotp_negative_order(sample):  # pylint: disable=invalid-name
+    """
+    Check that passing negative values for the 'order' in k.p construction
+    raises an error.
+    """
     model_tb = tbmodels.io.load(sample('InAs_nosym.hdf5'))
     with pytest.raises(ValueError):
         model_tb.construct_kdotp((0, 0, 0), order=-1)
