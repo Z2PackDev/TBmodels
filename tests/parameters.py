79747a0a9b1783494fef9ca6fac26bb794f741c4
Dominik Gresch <greschd@gmx.ch>
2020-01-20 13:03:51 +0100

    Fix prospector issues in arithmetic tests.

--- tests/parameters.py
+++ tests/parameters.py
@@ -3,6 +3,7 @@
 
 # (c) 2015-2018, ETH Zurich, Institut fuer Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
+"""Defines parameters used in different tests."""
 
 T_VALUES = [(t1, t2) for t1 in [-0.1, 0.2, 0.3] for t2 in [-0.2, 0.5]]
 KPT = [(0.1, 0.2, 0.7), (-0.3, 0.5, 0.2), (0., 0., 0.), (0.1, -0.9, -0.7)]
