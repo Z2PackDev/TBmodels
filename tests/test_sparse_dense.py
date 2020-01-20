086c4e2c7c9b8647ebbd007c2c97bb072155309f
Dominik Gresch <greschd@gmx.ch>
2020-01-20 14:11:43 +0100

    Fix pre-commit issues in sparse_dense tests.

--- tests/test_sparse_dense.py
+++ tests/test_sparse_dense.py
@@ -3,25 +3,35 @@
 
 # (c) 2015-2018, ETH Zurich, Institut fuer Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
+"""Tests for model sparsity handling / switching."""
 
 import pytest
-import tbmodels
 import numpy as np
+
+import tbmodels
 
 from parameters import T_VALUES, KPT
 
 
 @pytest.mark.parametrize('t1', T_VALUES)
 def test_simple(t1, get_model):
-    model1 = get_model(*t1, sparse=True)
-    model2 = get_model(*t1, sparse=False)
+    """
+    Check that a simple model set up as sparse and dense creates
+    the same Hamiltonians.
+    """
+    model_dense = get_model(*t1, sparse=True)
+    model_sparse = get_model(*t1, sparse=False)
 
     for k in KPT:
-        assert np.isclose(model1.hamilton(k), model2.hamilton(k)).all()
+        assert np.isclose(model_dense.hamilton(k), model_sparse.hamilton(k)).all()
 
 
 @pytest.mark.parametrize('t1', T_VALUES)
 def test_change_to_dense(t1, get_model, models_close):
+    """
+    Check that creating a sparse model and then switching it to dense
+    creates the same result as directly creating a dense model.
+    """
     model1 = get_model(*t1, sparse=True)
     model2 = get_model(*t1, sparse=False)
     model1.set_sparse(False)
@@ -30,6 +40,10 @@ def test_change_to_dense(t1, get_model, models_close):
 
 @pytest.mark.parametrize('t1', T_VALUES)
 def test_change_to_sparse(t1, get_model, models_close):
+    """
+    Check that creating a dense model and then switching it to sparse
+    creates the same result as directly creating a sparse model.
+    """
     model1 = get_model(*t1, sparse=True)
     model2 = get_model(*t1, sparse=False)
     model2.set_sparse(True)
@@ -38,9 +52,13 @@ def test_change_to_sparse(t1, get_model, models_close):
 
 @pytest.mark.parametrize('hr_name', ['hr_hamilton.dat', 'wannier90_hr.dat', 'wannier90_hr_v2.dat'])
 def test_hr(hr_name, sample):
+    """
+    Check that models loaded from *_hr.dat format have the same Hamiltonians
+    when loaded as either sparse or dense models.
+    """
     hr_file = sample(hr_name)
-    model1 = tbmodels.Model.from_hr_file(hr_file, occ=28, sparse=False)
-    model2 = tbmodels.Model.from_hr_file(hr_file, occ=28, sparse=True)
+    model1 = tbmodels.Model.from_wannier_files(hr_file=hr_file, occ=28, sparse=False)
+    model2 = tbmodels.Model.from_wannier_files(hr_file=hr_file, occ=28, sparse=True)
 
     for k in KPT:
         assert np.isclose(model1.hamilton(k), model2.hamilton(k)).all()
