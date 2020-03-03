ced04449ffbf0310284954cdf3a83ef93fef3578
Dominik Gresch <greschd@gmx.ch>
2020-03-03 14:02:21 +0100

    Add tests for the --sparsity CLI flag

--- tests/test_cli_symmetrize.py
+++ tests/test_cli_symmetrize.py
@@ -15,21 +15,25 @@ import tbmodels
 from tbmodels._cli import cli
 
 
-def test_cli_symmetrize(models_close, sample):
+def test_cli_symmetrize(
+    models_close, sample, cli_sparsity_arguments, modify_reference_model_sparsity
+):
     """
     Test the 'symmetrize' command.
     """
     runner = CliRunner()
     with tempfile.NamedTemporaryFile() as out_file:
         run = runner.invoke(
-            cli, [
+            cli,
+            [
                 'symmetrize', '-o', out_file.name, '-s',
                 sample('InAs_symmetries.hdf5'), '-i',
                 sample('InAs_nosym.hdf5')
-            ],
+            ] + cli_sparsity_arguments,
             catch_exceptions=False
         )
         print(run.output)
         model_res = tbmodels.Model.from_hdf5_file(out_file.name)
     model_reference = tbmodels.Model.from_hdf5_file(sample('InAs_sym_reference.hdf5'))
+    modify_reference_model_sparsity(model_reference)
     models_close(model_res, model_reference)
