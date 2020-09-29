f28d877ce643574674581e9ea2bfda1de57092fe
Dominik Gresch <greschd@gmx.ch>
2020-09-29 16:22:43 +0200

    Remove work-around for setuptools 'attr:'.
    The setuptools 'attr' can now load attributes defined at the
    module top-level without loading the module itself (since 46.4).
    As such, we no longer need the separate '_version.py' file, and
    instead require an up-to-date setuptools.

--- tbmodels/_version.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Helper module that defines the TBmodels version. The purpose of this
-module is to make the '__version__' available without any external
-dependencies, for the purposes of the build system.
-"""
-
-__version__ = "1.4.1"
